5ed7aadb8670e96a8bea5b0e0b9409e0fb988ab3
Junier <junierba@yahoo.co.uk>
2021-11-06 16:13:16 +0100

    Add files via upload

---
+++ Opdracht_11_Javascript_PowershellComandLine/script.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy students");
+
+//this is a large sum
+/* let total_1 = 1230941 * 1823794;
+
+let total_2 = 637263 / 54; */
+
+console.log(total_1);
+console.log(total_2);
+
+/* OUTPUT:
+
+2244982810154
+script.js:8 */
